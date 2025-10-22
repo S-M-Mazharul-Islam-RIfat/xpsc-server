d4c39898f17832a4072955e105e48dd3bca15a48
S-M-Mazharul-Islam-RIfat <smmjrifat@gmail.com>
2025-10-22 13:29:12 +0600

    update the docker file

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
    try {
-      // Connect the client to the server	(optional starting in v4.7)
+      // connect the client to the server	(optional starting in v4.7)
       // await client.connect();
 
       const clubUserCollection = client.db("xpscDB").collection("clubUsers");
